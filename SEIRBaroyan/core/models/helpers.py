52b10c9b3676f69fd35ac10b4fb6b0946ca1f641
neseleznev <ne.seleznev@gmail.com>
2017-04-05 02:16:46 +0200

    fix BUG

--- SEIRBaroyan/core/models/helpers.py
+++ SEIRBaroyan/core/models/helpers.py
@@ -8,8 +8,8 @@ def max_elem_index(iterable):
     return list_.index(max(list_))
 
 
-def calculate_tpeak_bias(x, y):
-    peak_index_real = max_elem_index(x)
-    peak_index_model = max_elem_index(y)
+def calculate_tpeak_bias(y_model, data):
+    peak_index_real = max_elem_index(data)
+    peak_index_model = max_elem_index(y_model)
     delta = peak_index_model - peak_index_real
     return delta
