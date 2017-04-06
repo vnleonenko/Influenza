a669b59f23c22e9b1d67e4f782bb68e27db518d2
neseleznev <ne.seleznev@gmail.com>
2017-04-06 03:50:14 +0200

    redesign collect_data2

--- SEIRBaroyan/collect_data2.py
+++ SEIRBaroyan/collect_data2.py
@@ -7,11 +7,13 @@
 
 import csv
 from functools import partial
+import itertools
+import os
 import time
 
 from scipy import array
 
-from core.methods import BaroyanSLSQPOptimizer
+from core.methods import BaroyanSLSQPOptimizer, BaroyanLBFGSBOptimizer, BaroyanTNCOptimizer
 from core.models.baroyan_rvachev import BaroyanParams
 from core.utils import get_flu_data, remove_background_incidence, get_filename_list, \
     get_population
@@ -23,45 +25,48 @@ __maintainer__ = "Nikita Seleznev (ne.seleznev@gmail.com)"
 INCIDENCE_ROOT = r'FLU_rjnamm_rev/FLU_%s/'
 POPULATION_CSV_FILE = r'input_population/population_%s.csv'
 DGTS_ROOT = 'benchmark/dgts/'
-CSV_FILE = DGTS_ROOT + '%s_%s.csv'
-PNG_FILE = DGTS_ROOT + 'graph/%s_%s.png'
+CSV_FILE = DGTS_ROOT + '%s/%s_%s_%s.csv'
+PNG_FILE = DGTS_ROOT + '%s/%s_%s_%s.png'
 
 
 class Params(BaroyanParams):
     N = 3000  # epid duration
     T = 8  # disease duration for a single person
-    SIZE = 5  # 25
+    SIZE = 25
     DISABLE_RANDOM = True
-    RANDOM_SEED = 4200
-    K_RANGE = (1.02, 1.6)
+    # RANDOM_SEED = 42
+    K_RANGE = (1.02, 1.4)
     I0_RANGE = (1.0, 1.0)  # (0.1, 100)
     TPEAK_BIAS_RANGE = range(-7, 7)  # (-3, 3)
 
 
-def fit(file, optimizer_cls, population, city_mark):
+def fit(args, population, city_mark):
+    file, optimizer_cls, params = args
     dates, y_real = get_flu_data(file)
     data = remove_background_incidence(y_real)
 
     logger_list = []
+    csv_filename = CSV_FILE % (file[-12:-8], city_mark, optimizer_cls.__name__, params.SIZE)
+    png_filename = PNG_FILE % (file[-12:-8], city_mark, optimizer_cls.__name__, params.SIZE)
 
-    with open(CSV_FILE % (city_mark, file[-12:-8]), "a+") as f:
+    os.makedirs(os.path.dirname(csv_filename), exist_ok=True)
+    with open(csv_filename, "w+") as f:
         reader = csv.reader(f)
         for idx, row in enumerate(reader):
             # print(str(idx) + ": " + str(row))
-            logger_list.append((float(element) for element in row))
+            logger_list.append(tuple(float(element) for element in row))
 
-    optimizer = optimizer_cls(data, population[file[-12:-8]], Params(), logger_list=logger_list)
+    optimizer = optimizer_cls(data, population[file[-12:-8]], params, logger_list=logger_list)
     optimizer.fit_one_outbreak()
     optimizer.get_results()
 
-    with open(CSV_FILE % (city_mark, file[-12:-8]), 'w+', newline='') as f:
+    with open(csv_filename, 'w+', newline='') as f:
         writer = csv.writer(f)
         for row in set(logger_list):
             writer.writerow(row)
 
     # Drawing
     data = array(logger_list)
-
     xs = data[:, 0]
     ys = data[:, 1]
     zs = data[:, 2]
@@ -84,10 +89,11 @@ def fit(file, optimizer_cls, population, city_mark):
         surf = mlab.pipeline.surface(mesh)
 
         # Simple plot.
-        mlab.xlabel("x")
-        mlab.ylabel("y")
-        mlab.zlabel("z")
-        mlab.show()
+        mlab.xlabel("K")
+        mlab.ylabel("shift")
+        mlab.zlabel("R^2")
+        mlab.show()  # TODO disable
+        mlab.savefig(png_filename, dpi=480)
     except ImportError:
         import sys
         print("Unable to Import mayavi. Drawing in matplotlib", file=sys.stderr)
@@ -97,7 +103,7 @@ def fit(file, optimizer_cls, population, city_mark):
         from matplotlib import cm
         from mpl_toolkits.mplot3d import Axes3D
 
-        fig = plt.figure()
+        fig = plt.figure(figsize=(12, 9))
         ax = fig.add_subplot(111, projection='3d')
 
         surf = ax.plot_trisurf(xs, ys, zs, cmap=cm.jet, linewidth=0)
@@ -107,10 +113,14 @@ def fit(file, optimizer_cls, population, city_mark):
         ax.yaxis.set_major_locator(MaxNLocator(6))
         ax.zaxis.set_major_locator(MaxNLocator(5))
 
+        ax.set_xlabel('K')
+        ax.set_ylabel('shift')
+        ax.set_zlabel('R^2')
+
         fig.tight_layout()
 
-        # plt.show()  # or:
-        fig.savefig(PNG_FILE % (city_mark, file[-12:-8]))
+        plt.show()  # TODO or:
+        fig.savefig(png_filename, dpi=480)
 
 
 def fit_safe(*args, **kwargs):
@@ -120,39 +130,47 @@ def fit_safe(*args, **kwargs):
         return e
 
 
-def invoke(files, optimizer_cls, population, city_mark, parallel=True, safe=True):
+def invoke(files, optimizers, params_list, population, city_mark, parallel=True, safe=True):
     if safe:
         function = fit_safe
     else:
         function = fit
 
     if parallel:
-        calc = partial(function, optimizer_cls=optimizer_cls, population=population, city_mark=city_mark)
+        calc = partial(function, population=population, city_mark=city_mark)
 
         import multiprocessing
         pool = multiprocessing.Pool(multiprocessing.cpu_count())
-        pool.map(calc, files)
+        pool.map(calc, itertools.product(files, optimizers, params_list))
         pool.close()
         pool.join()
 
     else:
-        for file in files:
-            function(file, optimizer_cls, population, city_mark)
+        for args in itertools.product(files, optimizers, params_list):
+            function(args, population, city_mark)
 
 
 def main():
-    city_mark = 'msk'  # for three cities ,'msk','nsk']
+    city_mark = 'spb'  # for three cities ,'msk','nsk']
     population = get_population(POPULATION_CSV_FILE % city_mark)
-    all_files = get_filename_list(INCIDENCE_ROOT % city_mark)
+    all_files = get_filename_list(INCIDENCE_ROOT % city_mark)[:1]
 
     # Filter 2000 only
     # all_files = [x for x in all_files if "2000" in x]
 
     # Filter first two
-    all_files = all_files[:2]
+    # all_files = all_files[:2]
+    params_list = []
+    for size in [1]:  # TODO , 10, 20]:
+        params = Params()
+        params.SIZE = size
+        params_list.append(params)
 
-    invoke(all_files, BaroyanSLSQPOptimizer,
-           population, city_mark, parallel=False, safe=False)
+    invoke(all_files,
+           [BaroyanSLSQPOptimizer],  # TODO, BaroyanLBFGSBOptimizer, BaroyanTNCOptimizer],
+           params_list,
+           population, city_mark,
+           parallel=True, safe=False)
 
 
 if __name__ == '__main__':
